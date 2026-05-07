77a7c15194305549133fcc5c105882999b270ccc
PallaviOlipi <Pallavi.vallepu@gmail.com>
2026-05-07 12:31:53 -0400

    Intial TYpescript Code

---
+++ Comments.ts
@@ -0,0 +1,9 @@
+//SIngle line comment
+console.log("This is a single line comment in TypeScript");
+
+/* multi line comments
+    This is a multi line comment in TypeScript
+    ```*/
+    console.log("This is a multi line comment in TypeScript");
+
+      console.log("Explain Java Script");
