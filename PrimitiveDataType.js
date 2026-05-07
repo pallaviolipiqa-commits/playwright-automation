6f77ff19325de0e09c9be4a79901dd0950a819ca
PallaviOlipi <Pallavi.vallepu@gmail.com>
2026-05-07 15:58:23 -0400

    PrimitiveDataTypes

---
+++ PrimitiveDataType.js
@@ -0,0 +1,20 @@
+"use strict";
+//Sample code demonstrating the use of primitive data types in TypeScript
+//String data type
+let userName = "Alice";
+//Number data type
+let userAge = 30;
+//Boolean data type
+let isAdmin = true;
+//Symbol data type
+let userSymbol = Symbol("user");
+//null data type
+let userNull = null;
+//undefined data type
+let userUndefined = undefined;
+console.log("User Name is", userName);
+console.log("User Age is", userAge);
+console.log("Is Admin?", isAdmin);
+console.log("User Symbol is", userSymbol.toString());
+console.log("User Null is", userNull);
+console.log("User Undefined is", userUndefined);
---
+++ PrimitiveDataType.ts
@@ -0,0 +1,30 @@
+//Sample code demonstrating the use of primitive data types in TypeScript
+
+//String data type
+let userName: string = "Alice"; 
+
+//Number data type
+let userAge: number = 30;
+
+//Boolean data type
+let isAdmin: boolean = true;
+
+//Symbol data type
+let userSymbol: symbol = Symbol("user");   
+
+//null data type
+let userNull: null = null;  
+
+//undefined data type
+let userUndefined: undefined = undefined;
+
+
+
+console.log("User Name is",userName);
+
+console.log("User Age is",userAge);
+console.log("Is Admin?",isAdmin);
+console.log("User Symbol is",userSymbol.toString());
+console.log("User Null is",userNull);
+console.log("User Undefined is",userUndefined);
+
