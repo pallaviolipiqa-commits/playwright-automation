21ccd832269072f20ff10f45aa5a754baf0454d5
PallaviOlipi <Pallavi.vallepu@gmail.com>
2026-05-08 10:52:37 -0400

    ifStatement

---
+++ ifStatement.js
@@ -0,0 +1,14 @@
+"use strict";
+let marks = 35;
+if (marks >= 90 && marks <= 100) {
+    console.log("Grade: A");
+}
+else if (marks > 35 && marks < 90) {
+    console.log("Grade: B");
+}
+else if (marks <= 35 && marks >= 0) {
+    console.log("Grade: C");
+}
+else {
+    console.log("Enter Valid Marks");
+}
---
+++ ifStatement.ts
@@ -0,0 +1,13 @@
+
+//Find the marks of an student and print the grade based on the following criteria:
+let marks : number = 35;
+if (marks >= 90 && marks <= 100) {
+    console.log("Grade: A");
+} else if (marks > 35 && marks < 90) {
+    console.log("Grade: B");
+} else if (marks <= 35 && marks >=0) {
+    console.log("Grade: C");
+} else {
+    console.log("Enter Valid Marks");
+}   
+
